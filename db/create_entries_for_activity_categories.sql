5e202f409b572b6c26f001901a9906d74e015cde
Gabriel Crisan <gabicrisan01@gmail.com>
2026-01-03 13:10:18 +0100

    Refactor database table entries

--- db/create_entries_for_activity_categories.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO activity_categories (name, description)
-VALUES
-('Community Support', 'Activities focused on helping local communities through various support initiatives'),
-('Environmental Conservation', 'Projects aimed at protecting and preserving the natural environment'),
-('Educational Programs', 'Teaching and mentoring activities for various age groups'),
-('Animal Welfare', 'Care and protection of animals in need'),
-('Healthcare Assistance', 'Supporting healthcare services and patient care');
---
+++ db/delete_all_entries.sql
@@ -0,0 +1,10 @@
+TRUNCATE TABLE
+    volunteer_activities,
+    activities,
+    donations,
+    coordinators,
+    volunteers,
+    activity_categories,
+    ongs,
+    users
+RESTART IDENTITY CASCADE;
